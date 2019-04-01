5d03cb874694fc1b267348527eb6b671c1451947
HuaXingF <jiahuaxing@outlook.com>
2019-04-01 17:03:03 +0800

    提交

--- src/api/medical-theme-list.js
+++ src/api/medical-theme-list.js
@@ -5,20 +5,20 @@ import baseURL from '@/assets/js/config'
 //初始化本周数据湖数据总量变化图
 ///hive/selectTotalDay?tableName=total
 export const selectTotalDay = params => {
-    const url = `${baseURL}/hive/selectTotalDay?tableName=total`
+    const url = `${baseURL}/hive/selectTotalDay?name=total`
     return axios.post(url, { params: params })
 }
 //数据湖数据总量月变化趋势图
 //hive/selectTotalMonth?tableName=total
 export const selectTotalMonth = params => {
-    const url = `${baseURL}/hive/selectTotalMonth?tableName=total`
+    const url = `${baseURL}/hive/selectTotalMonth?name=total`
     return axios.post(url, { params: params })
 }
 //单表数据总量变化趋势 || 单表数据增量变化趋势 根据表名查询||周期查询
 ///hive/selectTotalWeek?tableName=
 export const selectTotal = params => {
     //console.log(params,"0000000000000000")
-    const url = `${baseURL}/hive/${params.value2}?tableName=${params.value1}`
+    const url = `${baseURL}/hive/${params.value2}?name=${params.value1}`
     return axios.post(url, { params: params })
 }
 
--- src/api/message-search.js
+++ src/api/message-search.js
@@ -4,21 +4,21 @@ import baseURL from '@/assets/js/config'
 
 // 本周核心数据库数据总量变化图
 export const selectotle_week = params => {
-    const url = `${baseURL}/tabAllRows/selectTotalDay?tableName=total`
+    const url = `${baseURL}/tabAllRows/selectTotalDay?name=total`
     return axios.post(url, { params: params })
 }
 //核心数据库数据总量月变化趋势图
 export const selectotle_month = params => {
-    const url = `${baseURL}/tabAllRows/selectTotalMonth?tableName=total`
+    const url = `${baseURL}/tabAllRows/selectTotalMonth?name=total`
     return axios.post(url, { params: params })
 }
 //核心库单表数据总量变化趋势
 export const selectotle_trend = params => {
-    const url = `${baseURL}/tabAllRows/${params.value1}?tableName=${params.value}`
+    const url = `${baseURL}/tabAllRows/${params.value1}?name=${params.value}`
     return axios.post(url, { params: params })
 }
 // 核心库表数据增量变化趋势
 export const select_trend = params => {
-    const url = `${baseURL}/tabAllRows/${params.value1}?tableName=${params.value}`
+    const url = `${baseURL}/tabAllRows/${params.value1}?name=${params.value}`
     return axios.post(url, { params: params })
 }
