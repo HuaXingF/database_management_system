b7329983f6e155db070a1137079c778df5fc7c5a
HuaXingF <jiahuaxing@outlook.com>
2019-04-01 14:58:25 +0800

    提交

--- src/views/medical-theme-manage/medical-theme-manage.vue
+++ src/views/medical-theme-manage/medical-theme-manage.vue
@@ -148,6 +148,7 @@
 import MTopNav from "@/components/m-topNav/m-topNav";
 import {
   selectDimRelatedCountSum
+
 } from "@/api/medical-theme-manage.js"
 export default {
   name: "medicalThemeManage",
