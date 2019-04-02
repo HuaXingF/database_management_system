3d00251b979badf5038f2aaca9ccadd492a06e4b
cong276602239 <276602239@qq.com>
2019-04-02 11:53:03 +0800

    wancong

--- src/router.js
+++ src/router.js
@@ -5,98 +5,139 @@ import Home from '@/views/home/home'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'hash',
-  base: process.env.BASE_URL,
-  linkActiveClass : 'is-active',
-  routes: [
-    {
-      path: '/',
-      redirect: '/SystemTime',
-      hidden: true
-    },
-    {
-      path: '/',
-      name: '',
-      iconCls: 'systemSetting',
-      component: Home,
-      leaf: true,
-      children: [
+    mode: 'hash',
+    base: process.env.BASE_URL,
+    linkActiveClass: 'is-active',
+    routes: [
         {
-          path: '/SystemTime',
-          name: '系统实时监控',
-          component: () => import(/* webpackChunkName: "systemSetting" */ '@/views/system-setting/system-setting.vue')
-        }
-      ]
-    },
-    {
-      path: '/',
-      name: '',
-      iconCls: 'medicalTheme',
-      component: Home,
-      leaf: true,
-      children: [
+            path: '/',
+            redirect: '/SystemTime',
+            hidden: true
+        },
         {
-          path: '/SystemDataLake',
-          name: '数据湖数据监控',
-          component: () => import(/* webpackChunkName: "messageSearch" */ '@/views/medical-theme-list/medical-theme-list.vue')
-        }
-      ]
-    },
-    {
-      path: '/',
-      name: '',
-      iconCls: 'messageSearch',
-      component: Home,
-      leaf: true,
-      children: [
+            path: '/',
+            name: '',
+            iconCls: 'systemSetting',
+            component: Home,
+            leaf: true,
+            flagHidden: false,
+            children: [
+                {
+                    path: '/SystemTime',
+                    name: '系统实时监控',
+                    component: () => import(/* webpackChunkName: "systemSetting" */ '@/views/system-setting/system-setting.vue')
+                }
+            ]
+        },
         {
-          path: '/SystemKernelData',
-          name: '核心数据库数据监控',
-          component: () => import(/* webpackChunkName: "messageSearch" */ '@/views/message-search/message-search.vue')
-        }
-      ]
-    },
-    {
-      path: '/',
-      name: '',
-      iconCls: 'expertLink',
-      component: Home,
-      leaf: true,
-      children: [
+            path: '/',
+            name: '',
+            iconCls: 'medicalTheme',
+            component: Home,
+            leaf: true,
+            flagHidden: false,
+            children: [
+                {
+                    path: '/SystemDataLake',
+                    name: '数据湖数据监控',
+                    component: () => import(/* webpackChunkName: "messageSearch" */ '@/views/medical-theme-list/medical-theme-list.vue')
+                }
+            ]
+        },
         {
-          path: '/SystemAssociation',
-          name: '数据关联信息监控',
-          component: () => import(/* webpackChunkName: "expertLink" */ '@/views/expert-link/expert-link.vue')
-        }
-      ]
-    },
-    {
-      path: '/',
-      name: '',
-      iconCls: 'handleIndexing',
-      component: Home,
-      leaf: true,
-      children: [
+            path: '/',
+            name: '',
+            iconCls: 'messageSearch',
+            component: Home,
+            leaf: true,
+            flagHidden: false,
+            children: [
+                {
+                    path: '/SystemKernelData',
+                    name: '核心数据库数据监控',
+                    component: () => import(/* webpackChunkName: "messageSearch" */ '@/views/message-search/message-search.vue')
+                }
+            ]
+        },
         {
-          path: '/SystemHistory',
-          name: '数据历史关联信息监控',
-          component: () => import(/* webpackChunkName: "expertLink" */ '@/views/medical-theme-manage/medical-theme-manage.vue')
-        }
-      ]
-    },
-    {
-      path: '/',
-      name: '',
-      iconCls: 'indexingCheck',
-      component: Home,
-      leaf: true,
-      children: [
+            path: '/',
+            name: '',
+            iconCls: 'expertLink',
+            component: Home,
+            leaf: true,
+            flagHidden: false,
+            children: [
+                {
+                    path: '/SystemAssociation',
+                    name: '数据关联信息监控',
+                    component: () => import(/* webpackChunkName: "expertLink" */ '@/views/expert-link/expert-link.vue')
+                }
+            ]
+        },
         {
-          path: '/indexingCheck',
-          name: '数据规则统计信息',
-          component: () => import(/* webpackChunkName: "indexingCheck" */ '@/views/indexing-check/indexing-check.vue')
-        }
-      ]
-    }
-  ]
+            path: '/',
+            name: '',
+            iconCls: 'handleIndexing',
+            component: Home,
+            leaf: true,
+            flagHidden: false,
+            children: [
+                {
+                    path: '/SystemHistory',
+                    name: '数据历史关联信息监控',
+                    component: () => import(/* webpackChunkName: "expertLink" */ '@/views/medical-theme-manage/medical-theme-manage.vue')
+                }
+            ]
+        },
+        {
+            path: '/',
+            name: '',
+            iconCls: 'indexingCheck',
+            component: Home,
+            leaf: true,
+            flagHidden: false,
+            children: [
+                {
+                    path: '/SystemRule',
+                    name: '数据规则管理',
+                    component: () => import(/* webpackChunkName: "indexingCheck" */ '@/views/indexing-check/indexing-check.vue')
+                }
+            ]
+        },
+        // {
+        //   path: 'SystemAddRule',
+        //   name: '新增数据规则',
+        //   component: () => import('./views/SystemAddRule')
+        // }
+        {
+            path: '/',
+            name: '',
+            iconCls: 'indexingCheck',
+            component: Home,
+            leaf: true,
+            flagHidden: false,
+            children: [
+                {
+                    path: '/SystemETL',
+                    name: 'ETL信息监控',
+                    component: () => import(/* webpackChunkName: "indexingCheck" */ '@/views/SystemETL/SystemETL.vue')
+                }
+            ]
+        },
+        {
+            path: '/',
+            name: '',
+            iconCls: 'indexingCheck',
+            component: Home,
+            leaf: true,
+            flagHidden: true,
+            children: [
+                {
+                    path: '/SystemAddRule',
+                    name: '新增数据规则',
+                    component: () => import('./views/SystemAddRule/SystemAddRule.vue')
+                }
+            ]
+        },
+    ]
 })
